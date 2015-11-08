868caf027f6802b0ef78382f50b2871988a6ab6f
Brian McIntosh <brian@edcite.com>
2015-11-08 14:28:22 -0800

    checkpoint 30 completed, interfaces

--- scripts/collection.js
+++ scripts/collection.js
@@ -1,28 +1,39 @@
+var buildCollectionItemTemplate = function() {
 
-var collectionItemTemplate =
-     '<div class="collection-album-container column fourth">'
-   + '  <img src="assets/images/album_covers/01.png"/>'
-   + '  <div class="collection-album-info caption">'
-   + '    <p>'
-   + '      <a class="album-name" href="/album.html"> The Colors </a>'
-   + '      <br/>'
-   + '      <a href="/album.html"> Pablo Picasso </a>'
-   + '      <br/>'
-   + '      X songs'
-   + '      <br/>'
-   + '    </p>'
-   + '  </div>'
-   + '</div>'
+  var template = '<div class="collection-album-container column fourth">' +
+  '  <img src="assets/images/album_covers/01.png"/>' +
+  '  <div class="collection-album-info caption">' +
+  '    <p>' +
+  '      <a class="album-name" href="album.html"> The Colors </a>' +
+  '      <br/>' +
+  '      <a href="album.html"> Pablo Picasso </a>' +
+  '      <br/>' +
+  '      X songs' +
+  '      <br/>' +
+  '    </p>' +
+  '  </div>' +
+  '</div>';
 
+  // Wrap variable in a jQuery object
+  return $(template);
+};
 
-window.onload = function() {
+$(window).load(function(){
 
-     var collectionContainer = document.getElementsByClassName('album-covers')[0];
+  // The container for our albums
+  var $collectionContainer = $('.album-covers');
 
-     collectionContainer.innerHTML = '';
- 
+  // Make sure the album container is empty
+  $collectionContainer.empty();
 
-     for (var i = 0; i < 12; i++) {
-         collectionContainer.innerHTML += collectionItemTemplate;
-     }
- }
+  // Loop through 12 albums
+  for(var i = 0; i < 12; i++){
+
+    var $newThumbnail = buildCollectionItemTemplate();
+
+    // Append the album template to the container 
+    $collectionContainer.append($newThumbnail);
+
+  }
+
+});
